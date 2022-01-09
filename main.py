d98292df6f143ba3a003bb22f1164e768cc8f444
Alex <webmaster@papagai.de>
2022-01-09 19:29:52 +0100

    create:

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def start():
-    return "Meine Webseite funktioniert"
+    return "<h1>Meine Webseite funktioniert</h1><h2>Etwas kleiner</h2>"
+
+
+@app.route("/home")
+def test():
+    return "<h1>Schon wieder</h1>"
+
+@app.route("/htmlseite")
+def htmlseite():
+    return render_template("index.html")   
+
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1")
+    # "127.0.0.1"-- lokale hosting --
+    #app.run(debug=True, host="0.0.0.0")
+    # "0.0.0.0" -- erstellung aktive ip adresse auf lokale hosting
